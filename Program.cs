13f8a432bead8ae38089f31af41c1319fed4d0d1
Daud Ali <adaud378@gmail.com>
2021-11-28 00:11:59 +0500

    hellow

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace prog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(1+8/2+((1*4)+(5*4))/4); 
+            Console.WriteLine((1+1+1+1)/2+(1+1+1)/3); 
+            Console.WriteLine(5*5+5/5+6); 
+            Console.WriteLine(((3+4)+(4*7))/5); 
+            Console.WriteLine((3*6*7*2)+12/2); 
+            Console.WriteLine(5-3*4%(6-1)); 
+            Console.WriteLine((8*4*2+6)/2+4); 
+        }
+    }
+}
